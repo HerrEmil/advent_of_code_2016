6946fc687c6dcbfc3495f2b2dfe2823bde45e436
Emil Andersson <vaccuum@gmail.com>
2016-12-15 17:17:04 +0100

    Day 09 perf, now OK-ish

--- HerrEmil-JavaScript/day-09.js
+++ HerrEmil-JavaScript/day-09.js
@@ -1,20 +1,20 @@
-// let fs = require('fs');
-// let input = fs.readFileSync('day-09-input.txt').toString();
-// console.log('Day 09, puzzle 1 solution: ' + decompress(input).length);
+const fs = require('fs');
+const input = fs.readFileSync('day-09-input.txt').toString();
+console.log('Day 09, puzzle 1 solution: ' + decompress(input).length);
 // console.log('Day 09, puzzle 2 solution: ' + decompressV2length(input));
 
 function getNextMarker(compressed) {
-	for (let i = 0; i < compressed.length; i += 1) {
-		if (compressed.charAt(i) == '(') {
-			let substring = compressed.substring(i);
-			let charCountIndex = 1;
-			let charCount = substring.substring(
-				charCountIndex, substring.indexOf('x'));
+	let i = 0;
+	const compressedLength = compressed.length;
+	for (; i < compressedLength; ++i) {
+		if (compressed.charAt(i) === '(') {
+			const substring = compressed.substring(i);
+			const charCount = substring.substring(1, substring.indexOf('x'));
 
 			if (parseInt(charCount) != NaN) {
-				let repitionCountIndex = substring.indexOf(charCount) +
+				const repitionCountIndex = substring.indexOf(charCount) +
 					charCount.length + 1;
-				let repitionCount = substring.substring(repitionCountIndex,
+				const repitionCount = substring.substring(repitionCountIndex,
 					substring.indexOf(')'));
 				if (parseInt(repitionCount) != NaN) {
 					return '(' + charCount + 'x' + repitionCount + ')';
@@ -25,17 +25,19 @@ function getNextMarker(compressed) {
 }
 
 function getNextCompressedString(compressed) {
-	let charCount = compressed.substring(compressed.indexOf('(') + 1,
+	const charCount = compressed.substring(compressed.indexOf('(') + 1,
 		compressed.indexOf('x'));
-	let repitionCount = compressed.substring(compressed.indexOf('x') + 1,
+	const repitionCount = compressed.substring(compressed.indexOf('x') + 1,
 		compressed.indexOf(')'));
 
-	let stringAfterMarker = compressed.substring(compressed.indexOf(')') + 1);
+	const stringAfterMarker = compressed.substring(compressed.indexOf(')') + 1);
 
 	let resultString = '';
+	const stringToRepeat = stringAfterMarker.substring(0, charCount);
 
-	for (let i = 0; i < repitionCount; i += 1) {
-		resultString += stringAfterMarker.substring(0, charCount);
+	let i = 0;
+	for (; i < repitionCount; ++i) {
+		resultString += stringToRepeat;
 	}
 
 	return resultString;
@@ -44,22 +46,24 @@ function getNextCompressedString(compressed) {
 function decompress(compressed) {
 	let decrompressed = '';
 
-	for (let i = 0; i < compressed.length; i += 1) {
-		let nextMarker = getNextMarker(compressed.substring(i));
-		if (nextMarker == undefined) {
+	let i = 0;
+	const compressedLength = compressed.length;
+	for (; i < compressedLength; ++i) {
+		const nextMarker = getNextMarker(compressed.substring(i));
+		if (nextMarker === undefined) {
 			// No more markers, copy rest of string and return
 			decrompressed += compressed.substring(i);
 			return decrompressed;
 		}
-		let stepsToNextMarker = compressed.substring(i).indexOf(nextMarker);
+		const stepsToNextMarker = compressed.substring(i).indexOf(nextMarker);
 		if (stepsToNextMarker > 0) {
 			decrompressed += compressed.substring(i, i + stepsToNextMarker);
 			i += stepsToNextMarker - 1;
 		}
-		if (stepsToNextMarker == 0) {
+		if (stepsToNextMarker === 0) {
 			decrompressed += getNextCompressedString(compressed.substring(i));
 			i += nextMarker.length; // jump over marker
-			let markerCharCount = parseInt(nextMarker.substring(
+			const markerCharCount = parseInt(nextMarker.substring(
 				nextMarker.indexOf('(') + 1, nextMarker.indexOf('x')));
 			i += markerCharCount; // jump over compressed string
 			i -= 1; // Hacky solution to every loop increasing by 1
@@ -69,31 +73,25 @@ function decompress(compressed) {
 	return decrompressed;
 }
 
-function decompressV2length(remainingCode) {
-	let decrompressedLength = 0;
+function decompressV2length(code) {
+	const nextMarker = getNextMarker(code);
+	const stepsToMarker = code.indexOf(nextMarker);
 
-	while (true) {
-		// console.log('remainingCode: ' + remainingCode);
-		let nextMarker = getNextMarker(remainingCode);
-		if (nextMarker == undefined) {
-			decrompressedLength += remainingCode.length;
-			return decrompressedLength;
-		}
-		let stepsToNextMarker = remainingCode.indexOf(nextMarker);
-		if (stepsToNextMarker > 0) {
-			decrompressedLength += stepsToNextMarker;
-			// remove stepsToNextMarker characters from start of remainingCode
-			remainingCode = remainingCode.substring(stepsToNextMarker);
-		}
-		if (stepsToNextMarker == 0) {
-			decrompressedLength += decompressV2length(
-				getNextCompressedString(remainingCode));
-			let markerCharCount = parseInt(nextMarker.substring(
-				nextMarker.indexOf('(') + 1, nextMarker.indexOf('x')));
-			// remove marker from start of remaining Code
-			remainingCode = remainingCode.substring(
-				nextMarker.length + markerCharCount);
-		}
+	if (stepsToMarker === 0) {
+		const length = decompressV2length(getNextCompressedString(code));
+		const markerCharCount = parseInt(nextMarker.substring(
+			nextMarker.indexOf('(') + 1, nextMarker.indexOf('x')));
+
+		return length + decompressV2length(code.substring(nextMarker.length +
+			markerCharCount));
+	}
+
+	if (stepsToMarker > 0) {
+		return stepsToMarker + decompressV2length(code.substring(stepsToMarker));
+	}
+
+	if (nextMarker === undefined) {
+		return code.length;
 	}
 }
 
