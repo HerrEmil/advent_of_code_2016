f73dd5ef2f060403cea9c64d84fcaf5377424918
Emil Andersson <vaccuum@gmail.com>
2016-12-23 10:31:09 +0100

    Day 11 perf changs, fast enough, test passes, wrong answer

--- HerrEmil-JavaScript/day-11.js
+++ HerrEmil-JavaScript/day-11.js
@@ -1,7 +1,7 @@
-// const fs = require('fs');
-// const input = fs.readFileSync('day-11-input.txt').toString().trim();
-// console.log('Day 11, puzzle 1 solution: ' +
-// 	getNumberOfSteps(input.split(/\r?\n/)));
+const fs = require('fs');
+const input = fs.readFileSync('day-11-input.txt').toString().trim();
+console.log('Day 11, puzzle 1 solution: ' +
+	getNumberOfSteps(input.split(/\r?\n/)));
 
 /* jshint esversion: 6 */
 
@@ -29,28 +29,30 @@ function getInitialState(instructions) {
 		for (let j = 0; j < floorLength; ++j) {
 			floors[i][j] = floors[i][j].replace(/a /g, '');
 			floors[i][j] = floors[i][j].replace(/-compatible/g, '');
+			floors[i][j] = floors[i][j].charAt(0) +
+				floors[i][j].substr(floors[i][j].indexOf(' ') + 1, 1);
 		}
 	}
 
-	return {
-		elevator: 0,
-		distance: 0,
-		floors: floors,
-	};
+	return [
+		0, // elevator position
+		pairsToFloors(floorsToPairs(floors)), // floors
+		0, // distance
+	];
 }
 
 function chipsFrying(floor) {
-	const chips = floor.filter((object) => object.includes('microchip'));
-	const generators = floor.filter((object) => object.includes('generator'));
+	const chips = floor.filter((object) => object.charAt(1) === 'm');
+	const generators = floor.filter((object) => object.charAt(1) === 'g');
 	let chipType = '';
 
 	if (generators.length > 0) {
 		const chipsLength = chips.length;
 		for (let i = 0; i < chipsLength; ++i) {
-			chipType = chips[i].substr(0, chips[i].indexOf(' '));
+			chipType = chips[i].charAt(0);
 
 			// Check if current chip has matching generator
-			if (!generators.some((g) => g.startsWith(chipType))) {
+			if (!generators.some((g) => g.charAt(0) === chipType)) {
 				return true;
 			}
 		}
@@ -59,13 +61,36 @@ function chipsFrying(floor) {
 	return false;
 }
 
-function getPairsString(state) {
+function pairsToFloors(pairs) {
+	let floors = [
+		[],
+		[],
+		[],
+		[],
+	];
+	let materialChars = ['a', 'b', 'c', 'd', 'e'];
+
+	for (let i = 0; i < pairs.length; ++i) {
+		let stringPair = pairs[i].toString();
+
+		if (pairs[i] < 10) {
+			stringPair = '0' + stringPair;
+		}
+
+		floors[stringPair.charAt(0)].push(materialChars[i] + 'g');
+		floors[stringPair.charAt(1)].push(materialChars[i] + 'm');
+	}
+
+	return floors;
+}
+
+function floorsToPairs(floors) {
 	let pairs = [];
 	let unpaired = {};
 
-	for (let i = 0; i < state.floors.length; ++i) {
-		for (let j = 0; j < state.floors[i].length; ++j) {
-			unpaired[state.floors[i][j]] = i;
+	for (let i = 0; i < floors.length; ++i) {
+		for (let j = 0; j < floors[i].length; ++j) {
+			unpaired[floors[i][j]] = i;
 		}
 	}
 
@@ -89,12 +114,12 @@ function getPairsString(state) {
 
 	joinedPairs.sort();
 
-	return pairs.join('');
+	return joinedPairs;
 }
 
 function validState(state) {
 	for (let i = 0; i < 4; ++i) {
-		if (chipsFrying(state.floors[i])) {
+		if (chipsFrying(state[1][i])) {
 			return false;
 		}
 	}
@@ -103,10 +128,10 @@ function validState(state) {
 }
 
 function getFollowingStates(state) {
-	state.distance += 1;
+	state[2] += 1;
 	let moves = [];
-	const numberOfObjects = state.floors[state.elevator].length;
-	const floor = state.elevator;
+	const numberOfObjects = state[1][state[0]].length;
+	const floor = state[0];
 	const above = floor + 1;
 	const below = floor - 1;
 	let nextState = JSON.parse(JSON.stringify(state));
@@ -114,17 +139,17 @@ function getFollowingStates(state) {
 	for (let i = 0; i < numberOfObjects; ++i) {
 		if (floor < 3) {
 			// Move one object up
-			nextState.elevator = above;
-			nextState.floors[above].push(nextState.floors[floor].splice(i, 1)[0]);
+			nextState[0] = above;
+			nextState[1][above].push(nextState[1][floor].splice(i, 1)[0]);
 			moves.push(nextState);
 			nextState = JSON.parse(JSON.stringify(state));
 
 			// Move two up (unless we're moving the last object on the floor)
 			if (i < numberOfObjects - 1) {
-				nextState.elevator = above;
-				let objects = nextState.floors[floor].splice(i, 2);
-				nextState.floors[above].push(objects[0]);
-				nextState.floors[above].push(objects[1]);
+				nextState[0] = above;
+				let objects = nextState[1][floor].splice(i, 2);
+				nextState[1][above].push(objects[0]);
+				nextState[1][above].push(objects[1]);
 				moves.push(nextState);
 				nextState = JSON.parse(JSON.stringify(state));
 			}
@@ -132,17 +157,17 @@ function getFollowingStates(state) {
 
 		if (floor > 0) {
 			// Move one object down
-			nextState.elevator = below;
-			nextState.floors[below].push(nextState.floors[floor].splice(i, 1)[0]);
+			nextState[0] = below;
+			nextState[1][below].push(nextState[1][floor].splice(i, 1)[0]);
 			moves.push(nextState);
 			nextState = JSON.parse(JSON.stringify(state));
 
 			// Move two down (unless we're moving the last object on the floor)
 			if (i < numberOfObjects - 1) {
-				nextState.elevator = below;
-				let objects = nextState.floors[floor].splice(i, 2);
-				nextState.floors[below].push(objects[0]);
-				nextState.floors[below].push(objects[1]);
+				nextState[0] = below;
+				let objects = nextState[1][floor].splice(i, 2);
+				nextState[1][below].push(objects[0]);
+				nextState[1][below].push(objects[1]);
 				moves.push(nextState);
 				nextState = JSON.parse(JSON.stringify(state));
 			}
@@ -156,40 +181,39 @@ function getFollowingStates(state) {
 }
 
 function isFinal(state) {
-	return state.elevator === 3 &&
-		state.floors[0].length === 0 &&
-		state.floors[1].length === 0 &&
-		state.floors[2].length === 0;
+	return state[0] === 3 &&
+		state[1][0].length === 0 &&
+		state[1][1].length === 0 &&
+		state[1][2].length === 0;
 }
 
 function getNumberOfSteps(instructions) {
-	let initialState = getInitialState(instructions);
-	let states = getFollowingStates(initialState);
+	const initial = getInitialState(instructions);
+	let states = getFollowingStates(initial);
 	let currentState = {};
 	let previousStates = [];
+	previousStates.push(initial[0] + floorsToPairs(initial[1]).join(''));
 
 	while (states.length > 0) {
 		currentState = states.splice(0, 1)[0];
 
 		if (isFinal(currentState)) {
-			return currentState.distance;
-		} else if (!previousStates.includes(currentState.elevator +
-				getPairsString(currentState))) {
+			return currentState[2];
+		} else if (!previousStates.includes(currentState[0] +
+				floorsToPairs(currentState[1]).join(''))) {
 			let followingStates = getFollowingStates(currentState);
 			let newStates = followingStates.filter(function(state) {
-				return !previousStates.includes(state.elevator + getPairsString(state));
+				return !previousStates.includes(state[0] +
+					floorsToPairs(state[1]).join(''));
 			});
-			previousStates.push(currentState.elevator + getPairsString(currentState));
-			previousStates.sort();
+			previousStates.push(currentState[0] +
+				floorsToPairs(currentState[1]).join(''));
 			states = states.concat(newStates);
 		}
 	}
+
 
 	return -1;
 }
 
 module.exports.getNumberOfSteps = getNumberOfSteps;
-
-
-// Pairs could be represented as a pair of floor numbers, i.e. [1,1], [2,3],
-// their names are not relevant
